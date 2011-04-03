27b468b244f04cdd97f0fe602d8f9d10d182f264
Cameron Finucane <cpf37@cornell.edu>
2011-04-02 20:59:49 -0400

    cleaned up a lot, made it into a class.  should be ready for general use now.

--- pyvicon.py
+++ pyvicon.py
@@ -1,21 +1,33 @@
-import socket
-import threading
-import array
-import sys
+import sys, math, time
+import socket, struct, threading
 
-# WARNING: THIS ASSUMES YOUR INTS ARE 4 BYTES LONG
-# TODO: FIX THIS ^^^
+#########################################################################
+## Simple module for retrieving data from a Vicon motion capture system
+## written by Cameron Finucane <cpf37@cornell.edu>
+#########################################################################
 
-class mysocket:
-    # from http://docs.python.org/howto/sockets.html
+class ViconStreamer:
+    # based on example from http://docs.python.org/howto/sockets.html
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._streamNames = None
+        self._desiredStreams = []
+        self._streaming = False
+        self._verbose = False
+        self.data = None
 
     def connect(self, host, port):
+        print ">> Connecting..."
         self.sock.connect((host, port))
+        print ">> Requesting stream info..."
+        # Mysteriously, the ordering of the following two bytes is the opposite
+        # of what the Vicon documentation claims it is
+        self._viconSend([1,0])
+        print ">> Receiving stream info..."
+        self._streamNames = s._viconReceive()
 
-    def mysend(self, msg):
+    def _send(self, msg):
         totalsent = 0
         while totalsent < len(msg):
             sent = self.sock.send(msg[totalsent:])
@@ -23,36 +35,104 @@ class mysocket:
                 raise RuntimeError("socket connection broken")
             totalsent = totalsent + sent
 
-    def myclose(self):
+    def close(self):
+        self.stopStreams()
+
+        print ">> Disconnecting..."
         self.sock.close()
 
-    def viconreceive(self):
+    def getData(self):
+        # returns None if no data is available yet
+
+        if self.data is None:
+            return None
+
+        return [self.data[i] for i in self._desiredStreams]
+
+    def printStreamInfo(self):
+        if self._streamNames is None:
+            raise RuntimeError("stream info is not available because you are not connected")
+        else:
+            print "Available streams:"
+            print "   " + "\n   ".join(["(%d) %s" % (i,n) for i,n in enumerate(self._streamNames)])
+
+    def selectStreams(self, names):
+        # For each name passed in, finds and subscribes to all streams whose name contains
+        # the input name.
+        # Returns the full stream names.
+
+        if self._streamNames is None:
+            raise RuntimeError("cannot set streams because you are not connected")
+
+        matchingStreamNames = []
+        self._desiredStreams = []
+        for m in names:
+            found = False
+            for i,n in enumerate(self._streamNames):
+                if m in n:
+                    matchingStreamNames.append(n)
+                    self._desiredStreams.append(i)
+                    found = True
+            if not found:
+                raise RuntimeError("could not find stream matching name '%s'" % m)
+
+        print ">> Subscribed to streams: " + ", ".join(matchingStreamNames)
+        return matchingStreamNames
+
+    def startStreams(self, verbose=False):
+        if self._desiredStreams == []:
+            raise RuntimeError("cannot start streaming because no streams are selected")
+
+        print ">> Starting streams..."
+        self._viconSend([3,0])
+
+        self._verbose = verbose
+        self._streaming = True
+        self.listenThread = threading.Thread(target = self._processStream)
+        self.listenThread.start()
+
+    def stopStreams(self):
+        if not self._streaming:
+            return
+
+        print ">> Stopping streams..."
+        self._streaming = False
+        self.listenThread.join()
+
+        self._viconSend([4,0])
+
+    def _viconSend(self, data):
+        msg = struct.pack('<' + str(len(data)) + 'L', *data)
+        self._send(msg)
+
+    def _viconReceive(self):
         # get header
-        msg = self.myreceive(2*4)
-        header = array.array('I',msg)
+        msg = self._receive(2*4)
+        header = struct.unpack("<2L", msg)
+
+        if header[0] not in [1,2]:
+            # packet we are not set up to process
+            return header
+
+        msg = self._receive(1*4)
+        length = struct.unpack("<1L", msg)
+
         if header[0] == 1:
             # info packet
-            msg = self.myreceive(1*4)
-            length = array.array('I',msg)
             strs = []
             for i in xrange(length[0]):
-                msg = self.myreceive(1*4)
-                strlen = array.array('I',msg)
-                msg = self.myreceive(strlen[0])
+                msg = self._receive(1*4)
+                strlen = struct.unpack("<1L", msg)
+                msg = self._receive(strlen[0])
                 strs.append(msg)
             return strs 
         elif header[0] == 2:  
             # data packet
-            msg = self.myreceive(1*4)
-            length = array.array('I',msg)
-            msg = self.myreceive(length[0]*8)
-            body = array.array('d',msg)
+            msg = self._receive(length[0]*8)
+            body = struct.unpack("<" + str(length[0]) + "d", msg)
             return body
-        else:
-            # something else
-            return header
 
-    def myreceive(self, msglen):
+    def _receive(self, msglen):
         msg = ''
         while len(msg) < msglen:
             chunk = self.sock.recv(msglen-len(msg))
@@ -61,61 +141,39 @@ class mysocket:
             msg = msg + chunk
         return msg
 
-def processStream(s,info):
-    ###########################################
-    #### you might want to edit this part! ####
-    ###########################################
-
-    #STREAMS_YOU_WANT = [32,33,34]
-    STREAMS_YOU_WANT = [i for (i,n) in enumerate(info) if "armRightUp <a" in n]
-
-    ###########################################
-    ###########################################
-
-    while KEEP_LISTENING:
-        data = s.viconreceive()
-        for i in STREAMS_YOU_WANT:
-            print info[i] , ": " , data[i], "  ", 
-        print
+    def _processStream(self):
+        while self._streaming:
+            self.data = s._viconReceive()
+            if self._verbose:
+                print "  ".join([self._streamNames[i] for i in self._desiredStreams])
+                for i in self._desiredStreams:
+                    print self.data[i], "  ",
+                print
 
 
-print ">> Connecting..."
-s = mysocket()
-s.connect("10.0.0.102", 800)
+if __name__ == "__main__":
 
-print ">> Requesting stream info..."
-init = array.array('I',[1,0]).tostring()
-s.mysend(init)
+    s = ViconStreamer()
+    s.connect("10.0.0.102", 800)
 
-print ">> Receiving stream info..."
-info = s.viconreceive()
+    if len(sys.argv) > 1 and sys.argv[1] in ["-l", "--list"]:
+        s.printStreamInfo()
+        sys.exit(0)
 
-if len(sys.argv) > 1 and sys.argv[1] == "-l":
-    print "Available streams:"
-    print "   " + "\n   ".join(["(%d) %s" % (i,n) for i,n in enumerate(info)])
-    sys.exit(0)
+    streams = s.selectStreams(["Time", "t-"])
+    
+    s.startStreams(verbose=False)
 
-print ">> Starting streams..."
-init = array.array('I',[3,0]).tostring()
-s.mysend(init)
+    try:
+        # Wait for first data to come in
+        while s.getData() is None: pass
 
-global KEEP_LISTENING
-KEEP_LISTENING = True
-listenThread = threading.Thread(target = processStream, args = (s,info))
-listenThread.start()
+        while True:
+            print "  ".join(streams)
+            print "  ".join(map(str,s.getData()))
+            time.sleep(0.01)
+    except KeyboardInterrupt:
+        pass
 
-try:
-    raw_input(" *** Press ENTER to stop! ***")
-except KeyboardInterrupt:
-    pass
-
-KEEP_LISTENING = False
-listenThread.join()
-
-print ">> Stopping streams..."
-init = array.array('I',[4,0]).tostring()
-s.mysend(init)
-
-print ">> Disconnecting..."
-s.myclose()
+    s.close()
 
